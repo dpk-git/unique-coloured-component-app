6df32ec0e684588d93afd0afa31ea37bfad60ca2
dpk <deepakvishwakarma416@gmail.com>
2021-03-17 17:41:13 +0530

    Improve performance and have new test cases

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Box = (props) => {
     // Create a div component and assign the given 
     // background color 
-    return <div
+    return <canvas
         // Set click handler to the div and pass a callback 
         style={{
             backgroundColor: props.color,
--- src/components/Helpers.test.js
+++ src/components/Helpers.test.js
@@ -1,8 +1,22 @@
 const idx = require('./Helpers.js')
 
 describe("Helper", ()=>{
-    it("Check methods", ()=>{
+    it("Color generation", ()=>{
         const colors = idx.generateColors();
         expect(colors).toContain('rgb(192,160,184)');
     });
+
+    it("Count No of Colors", ()=>{
+        const colors = idx.generateColors();
+        expect(colors.length).toEqual(32768)
+    });
+
+    it("Check for all unique elements", ()=>{
+        const colors = idx.generateColors();
+        const count=1;
+        colors.forEach(color=>{
+            expect(colors.filter(c=>c==color).length).toBe(1);
+        });
+    });
+
 });
